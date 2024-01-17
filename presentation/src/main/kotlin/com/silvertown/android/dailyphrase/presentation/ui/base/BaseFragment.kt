9b660eedcbe8902ea3fe6f2227406a48470567de
juhwankim-dev <juhwan.dev@gmail.com>
2024-01-18 01:35:45 +0900

    feat : baseFragment 생성

---
+++ presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/base/BaseFragment.kt
@@ -0,0 +1,31 @@
+package com.silvertown.android.dailyphrase.presentation.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+abstract class BaseFragment<VB : ViewBinding>(private val inflate: Inflate<VB>) : Fragment() {
+
+    private var _binding: VB? = null
+    val binding: VB
+        get() = checkNotNull(_binding) { "binding is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = inflate.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
