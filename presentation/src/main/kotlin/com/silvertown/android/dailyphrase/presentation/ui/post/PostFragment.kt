02425620feeda60f658e60e0a559e037cc5f532d
juhwankim-dev <juhwan.dev@gmail.com>
2024-01-18 01:38:13 +0900

    feat : 상세페이지 (post fragment) 생성

---
+++ presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/post/PostFragment.kt
@@ -0,0 +1,6 @@
+package com.silvertown.android.dailyphrase.presentation.ui.post
+
+import com.silvertown.android.dailyphrase.presentation.databinding.FragmentPostBinding
+import com.silvertown.android.dailyphrase.presentation.ui.base.BaseFragment
+
+class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::inflate)
