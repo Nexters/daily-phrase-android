c6b279064af270e33524962979eb6c01b63e059f
juhwankim-dev <juhwan.dev@gmail.com>
2024-01-20 23:07:22 +0900

    chore : 카카오 로그인 api 사용을 위한 설정

--- app/src/main/kotlin/com/silvertown/android/dailyphrase/DailyPhraseApplication.kt
+++ app/src/main/kotlin/com/silvertown/android/dailyphrase/DailyPhraseApplication.kt
@@ -1,6 +1,7 @@
 package com.silvertown.android.dailyphrase
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,5 +10,14 @@ class DailyPhraseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        initKakaoSdk()
+    }
+
+    private fun initKakaoSdk() {
+        KakaoSdk.init(
+            context = this,
+            appKey = BuildConfig.KAKAO_APP_KEY,
+        )
     }
 }
