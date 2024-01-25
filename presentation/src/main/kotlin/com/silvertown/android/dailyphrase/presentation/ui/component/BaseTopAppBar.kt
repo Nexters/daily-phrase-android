f0bb5f3c1106def10479273bea963d2d5af78886
Junhyeok <njh042777@gmail.com>
2024-01-26 00:26:27 +0900

    refactor : TopAppBar ArrowBack 통일

--- presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/component/BaseTopAppBar.kt
+++ presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/component/BaseTopAppBar.kt
@@ -1,6 +1,8 @@
 package com.silvertown.android.dailyphrase.presentation.ui.component
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
@@ -9,6 +11,7 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import com.silvertown.android.dailyphrase.presentation.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,7 +22,7 @@ fun BaseTopAppBar(
         containerColor = colorResource(id = R.color.white),
         titleContentColor = colorResource(id = R.color.black),
     ),
-    navigationContent: @Composable () -> Unit = {},
+    navigateToBack: () -> Unit = {},
     actionsContent: @Composable () -> Unit = {},
     titleContent: @Composable () -> Unit = {},
 ) {
@@ -29,7 +32,12 @@ fun BaseTopAppBar(
         modifier = modifier,
         colors = colors,
         navigationIcon = {
-            navigationContent()
+            IconButton(onClick = { navigateToBack() }) {
+                Icon(
+                    painterResource(id = R.drawable.ic_arrow_back_24),
+                    contentDescription = null
+                )
+            }
         },
         title = {
             titleContent()
