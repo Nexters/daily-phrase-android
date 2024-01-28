9a5fb22cd79ac1c6f45f5018efbc87a42bd8d84f
Junhyeok <njh042777@gmail.com>
2024-01-28 23:21:11 +0900

    feat : PostDatabase, Dao, Entity 정의

---
+++ data/src/main/kotlin/com/silvertown/android/dailyphrase/data/database/DaosModule.kt
@@ -0,0 +1,17 @@
+package com.silvertown.android.dailyphrase.data.database
+
+import com.silvertown.android.dailyphrase.data.database.PostDatabase
+import com.silvertown.android.dailyphrase.data.database.dao.PostDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DaosModule {
+    @Provides
+    fun providesPostDao(
+        database: PostDatabase,
+    ): PostDao = database.postDao()
+}
