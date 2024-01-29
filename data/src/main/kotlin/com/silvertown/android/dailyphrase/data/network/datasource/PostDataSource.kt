01b62712b63edbd4c1c86c01d1ef7c07816850dd
Junhyeok <njh042777@gmail.com>
2024-01-29 23:45:03 +0900

    feat : 글귀 목록 조회 data layer

--- data/src/main/kotlin/com/silvertown/android/dailyphrase/data/network/datasource/PostDataSource.kt
+++ data/src/main/kotlin/com/silvertown/android/dailyphrase/data/network/datasource/PostDataSource.kt
@@ -1,11 +1,11 @@
 package com.silvertown.android.dailyphrase.data.network.datasource
 
 import com.silvertown.android.dailyphrase.data.network.common.ApiResponse
-import com.silvertown.android.dailyphrase.data.network.model.response.PostsResponse
+import com.silvertown.android.dailyphrase.data.network.model.response.BasePostResponse
 
 interface PostDataSource {
     suspend fun getPosts(
         page: Int,
-        limit: Int,
-    ): ApiResponse<PostsResponse>
+        size: Int,
+    ): ApiResponse<BasePostResponse>
 }
