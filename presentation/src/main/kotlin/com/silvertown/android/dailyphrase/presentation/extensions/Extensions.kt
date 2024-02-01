bd118c6391e3515eb77ad6121525c4dd1a51bef3
Junhyeok <njh042777@gmail.com>
2024-02-01 23:09:53 +0900

    feat : navigateToStart Ext

--- presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/extensions/Extensions.kt
+++ presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/extensions/Extensions.kt
@@ -1,8 +1,22 @@
 package com.silvertown.android.dailyphrase.presentation.extensions
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import com.silvertown.android.dailyphrase.presentation.MainActivity
+import com.silvertown.android.dailyphrase.presentation.R
 
 fun Int.dpToPx(context: Context): Int {
     val density = context.resources.displayMetrics.density
     return (this * density + 0.5f).toInt()
+}
+
+fun Context.navigateToStart(
+    message: Int,
+) {
+    Toast.makeText(this, this.getString(message), Toast.LENGTH_SHORT).show()
+    val intent = Intent(this, MainActivity::class.java)
+    this.startActivity(intent)
+    (this as? Activity)?.finish()
 }
