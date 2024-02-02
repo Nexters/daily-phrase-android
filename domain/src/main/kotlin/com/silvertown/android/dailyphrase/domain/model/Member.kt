7c07d8aa3e98a3ce2a8adfb591c6b2a2d3435dd7
Junhyeok <njh042777@gmail.com>
2024-02-03 00:09:56 +0900

    feat : preferences, Member Default 값 설정

--- domain/src/main/kotlin/com/silvertown/android/dailyphrase/domain/model/Member.kt
+++ domain/src/main/kotlin/com/silvertown/android/dailyphrase/domain/model/Member.kt
@@ -1,9 +1,18 @@
 package com.silvertown.android.dailyphrase.domain.model
 
 data class Member(
-    val id: Long,
-    val name: String,
-    val email: String,
-    val imageUrl: String,
-    val quitAt: String,
-)
+    val id: Long = DEFAULT_ID.toLong(),
+    val name: String = DEFAULT_NAME,
+    val imageUrl: String = DEFAULT_IMAGE_URL,
+    val email: String = DEFAULT_EMAIL,
+    val quitAt: String = DEFAULT_QUIT_AT,
+) {
+    companion object {
+        const val DEFAULT_ID = 0
+        const val DEFAULT_NAME = "User"
+        const val DEFAULT_IMAGE_URL =
+            "https://cdn.pixabay.com/photo/2015/06/25/04/50/hand-print-820913_1280.jpg"
+        const val DEFAULT_EMAIL = ""
+        const val DEFAULT_QUIT_AT = ""
+    }
+}
