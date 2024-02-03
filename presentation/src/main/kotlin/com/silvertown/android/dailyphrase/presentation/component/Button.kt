9ff1c73189f994dba4b6e2a192354b761e8d5e0c
Junhyeok <njh042777@gmail.com>
2024-02-03 10:28:54 +0900

    refactor : Row padding end 추가

--- presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/component/Button.kt
+++ presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/component/Button.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -91,6 +92,7 @@ fun SpeakButton(
 
     Row(
         modifier = Modifier
+            .padding(end = 10.dp)
             .clickable(enabled = isClickable) {
                 speakTTS(context, message)
                 isClickable = false
