d264679ee050bda1fa079480d1f08bf51ed106e3
Junhyeok <njh042777@gmail.com>
2024-02-04 19:03:35 +0900

    feat : Detail, NonLogin 로그인 리스너 등록

--- presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/detail/DetailFragment.kt
+++ presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/detail/DetailFragment.kt
@@ -2,14 +2,20 @@ package com.silvertown.android.dailyphrase.presentation.ui.detail
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.compose.ui.Modifier
 import androidx.navigation.fragment.findNavController
+import com.silvertown.android.dailyphrase.presentation.MainActivity
 import com.silvertown.android.dailyphrase.presentation.base.BaseFragment
 import com.silvertown.android.dailyphrase.presentation.databinding.FragmentDetailBinding
+import com.silvertown.android.dailyphrase.presentation.util.LoginResultListener
 
-class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
+class DetailFragment :
+    BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate),
+    LoginResultListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (activity as MainActivity).setLoginResultListener(this)
 
         binding.composeView.setContent {
             DetailScreen(
@@ -19,5 +25,9 @@ class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding
                 }
             )
         }
+    }
+
+    override fun onLoginSuccess() {
+        Toast.makeText(requireContext(), "로그인 성공! 다시 시도 해보세요.", Toast.LENGTH_SHORT).show()
     }
 }
--- presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/mypage/non_login/NonLoginFragment.kt
+++ presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/mypage/non_login/NonLoginFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.compose.ui.Modifier
 import androidx.navigation.fragment.findNavController
+import com.silvertown.android.dailyphrase.presentation.MainActivity
 import com.silvertown.android.dailyphrase.presentation.databinding.FragmentNonLoginBinding
 import com.silvertown.android.dailyphrase.presentation.base.BaseFragment
+import com.silvertown.android.dailyphrase.presentation.util.LoginResultListener
 
-class NonLoginFragment : BaseFragment<FragmentNonLoginBinding>(FragmentNonLoginBinding::inflate) {
+class NonLoginFragment :
+    BaseFragment<FragmentNonLoginBinding>(FragmentNonLoginBinding::inflate),
+    LoginResultListener {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (activity as MainActivity).setLoginResultListener(this)
 
         binding.composeView.setContent {
             NonLoginScreen(
@@ -17,8 +23,17 @@ class NonLoginFragment : BaseFragment<FragmentNonLoginBinding>(FragmentNonLoginB
                 navigateToBack = {
                     findNavController().popBackStack()
                 },
-                onClickKaKaoLogin = { }
+                onClickKaKaoLogin = {
+                    (activity as MainActivity).kakaoLogin()
+                }
             )
         }
     }
+
+    override fun onLoginSuccess() {
+        NonLoginFragmentDirections
+            .moveToMyPageFragment()
+            .also { findNavController().navigate(it) }
+    }
+
 }
