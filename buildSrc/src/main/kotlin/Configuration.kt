146460eacc890837226d2608272e00c39bd20645
juhwankim-dev <juhwan.dev@gmail.com>
2024-02-09 21:25:56 +0900

    version up (1.0.1 -> 1.0.2)

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,10 +1,10 @@
 object Configuration {
     private const val majorVersion = 1
     private const val minorVersion = 0
-    private const val patchVersion = 0
+    private const val patchVersion = 2
     const val compileSdk = 34
     const val targetSdk = 34
     const val minSdk = 23
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 1
+    const val versionCode = 3
 }
