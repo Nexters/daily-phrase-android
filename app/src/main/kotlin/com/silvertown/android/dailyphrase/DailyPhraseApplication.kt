1818b0c45111ab6e8187929dd1486dde4473e6b6
Junhyeok <njh042777@gmail.com>
2024-04-07 17:54:48 +0900

    feat : messagingInitializer Apllication 등록

--- app/src/main/kotlin/com/silvertown/android/dailyphrase/DailyPhraseApplication.kt
+++ app/src/main/kotlin/com/silvertown/android/dailyphrase/DailyPhraseApplication.kt
@@ -4,18 +4,24 @@ import android.app.Application
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
 import com.kakao.sdk.common.KakaoSdk
+import com.silvertown.android.dailyphrase.messaging.initializers.MessagingInitializer
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class DailyPhraseApplication : Application() {
+
+    @Inject
+    lateinit var messagingInitializer: MessagingInitializer
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
 
         initKakaoSdk()
         initAdmobSetting()
-
+        messagingInitializer.initialize()
     }
 
     private fun initKakaoSdk() {
