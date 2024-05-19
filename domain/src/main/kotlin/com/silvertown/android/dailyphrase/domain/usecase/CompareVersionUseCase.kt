b02fa61735bb80737530bb42a8d4ed59c45d0f1a
juhwankim-dev <juhwan.dev@gmail.com>
2024-05-19 22:30:01 +0900

    refactor : 함수를 중첩해서 사용하는 코드를 중첩하지 않는 방향으로 수정 (준혁이 피드백)

--- domain/src/main/kotlin/com/silvertown/android/dailyphrase/domain/usecase/CompareVersionUseCase.kt
+++ domain/src/main/kotlin/com/silvertown/android/dailyphrase/domain/usecase/CompareVersionUseCase.kt
@@ -10,29 +10,6 @@ class CompareVersionUseCase @Inject constructor(
         needUpdateVersion: String,
         forceUpdateVersion: String,
     ): UpdateStatus {
-        fun isValidVersionForm(version: String): Boolean {
-            if (version.isEmpty()) {
-                return false
-            }
-
-            if (version.split(".").size != 3) {
-                return false
-            }
-
-            return version
-                .replace(".", "")
-                .toIntOrNull()
-                .let { it != null }
-        }
-
-        fun String.isGreaterVersion(other: String): Boolean {
-            val t = this.split(".").map { it.toInt() }
-            val o = other.split(".").map { it.toInt() }
-            val thisVersion = KotlinVersion(t[0], t[1], t[2])
-
-            return thisVersion.isAtLeast(o[0], o[1], o[2])
-        }
-
         if (!isValidVersionForm(appVersion) || !isValidVersionForm(needUpdateVersion) || !isValidVersionForm(forceUpdateVersion)) {
             return UpdateStatus.NOT_NEED_UPDATE // 업데이트 X
         }
@@ -44,6 +21,29 @@ class CompareVersionUseCase @Inject constructor(
         } else {
             UpdateStatus.FORCE_UPDATE // 최소 버전보다 낮은 경우 (강제 업데이트)
         }
+    }
+
+    private fun isValidVersionForm(version: String): Boolean {
+        if (version.isEmpty()) {
+            return false
+        }
+
+        if (version.split(".").size != 3) {
+            return false
+        }
+
+        return version
+            .replace(".", "")
+            .toIntOrNull()
+            .let { it != null }
+    }
+
+    private fun String.isGreaterVersion(other: String): Boolean {
+        val t = this.split(".").map { it.toInt() }
+        val o = other.split(".").map { it.toInt() }
+        val thisVersion = KotlinVersion(t[0], t[1], t[2])
+
+        return thisVersion.isAtLeast(o[0], o[1], o[2])
     }
 
     enum class UpdateStatus {
