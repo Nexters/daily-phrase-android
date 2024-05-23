0f013413a38d36b0579ceae7d33f467b82cfde2f
Junhyeok <njh042777@gmail.com>
2024-05-23 22:42:24 +0900

    refactor : 테이블명 상수화

--- data/src/main/kotlin/com/silvertown/android/dailyphrase/data/database/dao/RemoteKeysDao.kt
+++ data/src/main/kotlin/com/silvertown/android/dailyphrase/data/database/dao/RemoteKeysDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.silvertown.android.dailyphrase.data.database.model.RemoteKeys
+import com.silvertown.android.dailyphrase.data.database.model.RemoteKeys.Companion.REMOTE_KEYS_TABLE_NAME
 
 @Dao
 interface RemoteKeysDao {
@@ -12,9 +13,9 @@ interface RemoteKeysDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(remoteKeysList: List<RemoteKeys>)
 
-    @Query("SELECT * FROM remote_keys WHERE id = :id")
+    @Query("SELECT * FROM $REMOTE_KEYS_TABLE_NAME WHERE id = :id")
     suspend fun getRemoteKeys(id: Long): RemoteKeys?
 
-    @Query("DELETE FROM remote_keys")
+    @Query("DELETE FROM $REMOTE_KEYS_TABLE_NAME")
     suspend fun clearRemoteKeys()
 }
--- data/src/main/kotlin/com/silvertown/android/dailyphrase/data/database/model/RemoteKeys.kt
+++ data/src/main/kotlin/com/silvertown/android/dailyphrase/data/database/model/RemoteKeys.kt
@@ -2,10 +2,15 @@ package com.silvertown.android.dailyphrase.data.database.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.silvertown.android.dailyphrase.data.database.model.RemoteKeys.Companion.REMOTE_KEYS_TABLE_NAME
 
-@Entity(tableName = "remote_keys")
+@Entity(tableName = REMOTE_KEYS_TABLE_NAME)
 data class RemoteKeys(
     @PrimaryKey val id: Long,
     val prevKey: Int?,
     val nextKey: Int?,
-)
+) {
+    companion object {
+        const val REMOTE_KEYS_TABLE_NAME = "remote_keys"
+    }
+}
