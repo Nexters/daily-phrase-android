4f139eea4bfb328f702a54cf0729fe6833cd17b3
juhwankim-dev <juhwan.dev@gmail.com>
2024-07-22 22:58:18 +0900

    feat: 번호를 다 입력하면 버튼이 활성화 되도록 구현
    추가로 13자 까지만 입력이 가능하게 하였음

--- presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/event/WinningBottomSheet.kt
+++ presentation/src/main/kotlin/com/silvertown/android/dailyphrase/presentation/ui/event/WinningBottomSheet.kt
@@ -3,14 +3,26 @@ package com.silvertown.android.dailyphrase.presentation.ui.event
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.addTextChangedListener
 import com.silvertown.android.dailyphrase.presentation.base.BaseDialogFragment
 import com.silvertown.android.dailyphrase.presentation.databinding.BottomSheetWinningBinding
 
 class WinningBottomSheet : BaseDialogFragment<BottomSheetWinningBinding>(BottomSheetWinningBinding::inflate) {
     private val dimAmount = 0.45f
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.etPhoneNumber.addTextChangedListener {
+            binding.tvInputPhoneNumber.isEnabled = (it.toString().length == 13)
+        }
+    }
 
     override fun onStart() {
         super.onStart()
