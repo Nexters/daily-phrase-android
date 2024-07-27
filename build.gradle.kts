aaf1b42f96de6c540cc8ff2fbbeac2946d78fa4c
Junhyeok <njh042777@knu.ac.kr>
2024-07-27 10:15:28 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.android) apply falseㅇㅁㄴㅇ
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
